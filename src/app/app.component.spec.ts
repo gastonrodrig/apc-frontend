a4a6023a1df61b6878a386204ca06241a9692937
Christian Saavedra <chrissaavedra2002@outlook.es>
2024-11-13 00:39:50 -0500

    update v18

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sistema-clubes-frontend'`, () => {
+  it(`should have as title 'apc-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sistema-clubes-frontend');
+    expect(app.title).toEqual('apc-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sistema-clubes-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('apc-frontend app is running!');
   });
 });
